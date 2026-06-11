be046ab426a599daa2aa7ecc2965799037a760a1
i-am-rishbh <contactrishbhsharma@gmail.com>
2026-06-11 08:13:34 +0530

    Using Route_based and Query Based Approach

--- FleetManagement/Controllers/DevicesController.cs
+++ FleetManagement/Controllers/DevicesController.cs
@@ -19,31 +19,61 @@ namespace FleetManagement.Controllers
             new Device { DeviceID = "D-004", DeviceType = "Front_Radar", HW_Version = "v2.5", FirmwareVersion = "v1.5.0", Status = "Inactive", LastUpdated = DateTime.Now.AddDays(+3) }
         };
 
+        #region Route-Based Approach
+
+        //[HttpGet]
+        //public IActionResult GetAllDevices()
+        //{
+        //    return Ok(_devices);
+        //}
+
+        //[HttpGet("status/{status}")]
+        //public IActionResult GetDeviceStatus(string status)
+        //{
+        //    var deviceStatus = _devices.FindAll(x => x.Status == status);
+        //    var deviceType = _devices.FindAll(x => x.DeviceType == status);
+        //    if (deviceStatus == null)
+        //        return NotFound($"Device with status {status} not found.");
+        //    return Ok(deviceStatus);
+        //}
+
+        //[HttpGet("type/{deviceType}")]
+        //public IActionResult GetDeviceByType(string deviceType)
+        //{
+        //    var device = _devices.FirstOrDefault(x => x.DeviceType == deviceType);
+        //    if (device == null)
+        //        return NotFound($"Device with type {deviceType} not found.");
+        //    return Ok(device);
+        //}
+
+        #endregion
+        #region Query_Based Approach
+        
         [HttpGet]
-        public IActionResult GetAllDevices()
+        public IActionResult GetDevices([FromQuery] string status, [FromQuery] string deviceType)
         {
-            return Ok(_devices);
+            if (!string.IsNullOrEmpty(status))
+            {
+                var deviceStatus = _devices.FindAll(x => x.Status == status);
+                if (deviceStatus.Count == 0)
+                    return NotFound($"No devices found with status {status}.");
+                return Ok(deviceStatus);
+            }
+            else if (!string.IsNullOrEmpty(deviceType))
+            {
+                var deviceTypeDevices = _devices.FindAll(x => x.DeviceType == deviceType);
+                if (deviceTypeDevices.Count == 0)
+                    return NotFound($"No devices found with type {deviceType}.");
+                return Ok(deviceTypeDevices);
+            }
+            else
+            {
+                // If no query parameters are provided, return all devices
+                return Ok(_devices);
+            }
         }
 
-        [HttpGet("{status}")]
-        public IActionResult GetDeviceStatus(string status)
-        {
-            var deviceStatus = _devices.FindAll(x => x.Status == status);
-            var deviceType = _devices.FindAll(x => x.DeviceType == status);
-            if (deviceStatus == null)
-                return NotFound($"Device with status {status} not found.");
-            return Ok(deviceStatus);
-        }
-
-        [HttpGet("{deviceType}")]
-        public IActionResult GetDeviceByType(string deviceType)
-        {
-            var device = _devices.FirstOrDefault(x => x.DeviceType == deviceType);
-            if (device == null)
-                return NotFound($"Device with type {deviceType} not found.");
-            return Ok(device);
-        }
-
+        #endregion
     }
-   
+
 }
